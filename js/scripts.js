4b6e04f4e293868eb439700e6069a5d0424e4188
Your Name <you@example.com>
2017-10-26 15:13:38 +0200

    Add files to project

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+var triangle1Area = getTriangleArea(9, 3);
+console.log(triangle1Area);
+
+var triangle2Area = getTriangleArea(14, 7);
+console.log(triangle2Area);
+
+var triangle3Area = getTriangleArea(2, 15);
+console.log(triangle3Area);
+
 function getTriangleArea(a, h) {
     if (a <= 0) {
       return 'incorrect data';
